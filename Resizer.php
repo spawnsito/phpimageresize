6e0d468268413e9c4daeac5636cfa4828576e525
Sergio Revilla <replymusic@gmail.com>
2015-01-26 13:45:44 +0100

    snapshot at Mon Jan 26 13:33:38 CET 2015

--- Resizer.php
+++ Resizer.php
@@ -4,7 +4,6 @@
 class Resizer {
 
     public function __construct($path, $configuration=null) {
-        if (!($path instanceof ImagePath))
-            throw new InvalidArgumentException();
+        if (!($path instanceof ImagePath)) throw new InvalidArgumentException();
     }
 }
