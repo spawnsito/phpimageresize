afc430ec059b61d5270f737958e50b51a223fc62
Sergio Revilla <replymusic@gmail.com>
2015-01-26 11:51:17 +0100

    snapshot at Mon Jan 26 10:51:59 CET 2015

--- test/FunctionResizeTest.php
+++ test/FunctionResizeTest.php
@@ -37,6 +37,7 @@ class FunctionResizeTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($defaults, $asHash);
         $this->assertEquals($defaults, $nullOptions->asHash());
         $this->assertTrue($configured['thumbnail']);
+        $this->assertTrue($configured['maxOnly']);
     }
 }
 
