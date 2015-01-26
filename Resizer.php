d71df21527b7cded3318978c19a9999433687f06
Sergio Revilla <replymusic@gmail.com>
2015-01-26 14:09:49 +0100

    snapshot at Mon Jan 26 13:33:38 CET 2015

--- Resizer.php
+++ Resizer.php
@@ -22,7 +22,7 @@ class Resizer {
             $download_image = true;
             if(file_exists($local_filepath)):
                 $opts = $this->configuration->asHash();
-                if(filemtime($local_filepath) < strtotime('+'.$this->configuration->asHash()['cache_http_minutes'].' minutes')):
+                if(filemtime($local_filepath) < strtotime('+'.$opts['cache_http_minutes'].' minutes')):
                     $download_image = false;
                 endif;
             endif;
