5bf047ffba2855e0864e0a3bdde08eb8d05e2ea7
Sergio Revilla <replymusic@gmail.com>
2015-01-26 11:53:42 +0100

    snapshot at Mon Jan 26 10:51:59 CET 2015

--- test/FunctionResizeTest.php
+++ test/FunctionResizeTest.php
@@ -3,6 +3,20 @@
 include 'Options.php';
 
 class FunctionResizeTest extends PHPUnit_Framework_TestCase {
+
+    $defaults = array(
+        'crop' => false,
+        'scale' => 'false',
+        'thumbnail' => false,
+        'maxOnly' => false,
+        'canvas-color' => 'transparent',
+        'output-filename' => false,
+        'cacheFolder' => './cache/',
+        'remoteFolder' => './cache/remote/',
+        'quality' => 90,
+        'cache_http_minutes' => 20
+    );
+
     public function testOpts()
     {
         $this->assertInstanceOf('Options', new Options);
