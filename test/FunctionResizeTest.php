606d14f654da61c0f07b206215d3bcd49032e01b
Sergio Revilla <replymusic@gmail.com>
2015-01-26 11:19:23 +0100

    snapshot at Mon Jan 26 10:51:59 CET 2015

--- test/FunctionResizeTest.php
+++ test/FunctionResizeTest.php
@@ -7,6 +7,14 @@ class FunctionResizeTest extends PHPUnit_Framework_TestCase {
     {
         $this->assertInstanceOf('Options', new Options);
     }
+
+    public function testDefaults() {
+        $options = new Options();
+        $defaults = array('crop' => false, 'scale' => 'false', 'thumbnail' => false, 'maxOnly' => false,
+            'canvas-color' => 'transparent', 'output-filename' => false,
+            'cacheFolder' => $cacheFolder, 'remoteFolder' => $remoteFolder, 'quality' => 90, 'cache_http_minutes' => 20);
+
+    }
 }
 
 ?>
