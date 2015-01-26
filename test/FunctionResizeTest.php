126ac3b58dcf8f8fa0cee1fa6bc889250a639ecc
Sergio Revilla <replymusic@gmail.com>
2015-01-26 11:19:46 +0100

    snapshot at Mon Jan 26 10:51:59 CET 2015

--- test/FunctionResizeTest.php
+++ test/FunctionResizeTest.php
@@ -10,9 +10,18 @@ class FunctionResizeTest extends PHPUnit_Framework_TestCase {
 
     public function testDefaults() {
         $options = new Options();
-        $defaults = array('crop' => false, 'scale' => 'false', 'thumbnail' => false, 'maxOnly' => false,
-            'canvas-color' => 'transparent', 'output-filename' => false,
-            'cacheFolder' => $cacheFolder, 'remoteFolder' => $remoteFolder, 'quality' => 90, 'cache_http_minutes' => 20);
+        $defaults = array(
+            'crop' => false,
+            'scale' => 'false',
+            'thumbnail' => false,
+            'maxOnly' => false,
+            'canvas-color' => 'transparent',
+            'output-filename' => false,
+            'cacheFolder' => $cacheFolder,
+            'remoteFolder' => $remoteFolder,
+            'quality' => 90,
+            'cache_http_minutes' => 20
+        );
 
     }
 }
