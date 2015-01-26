8bdfeef0fc3769fba6fa58e9188668e0c786ee64
Sergio Revilla <replymusic@gmail.com>
2015-01-26 11:15:19 +0100

    snapshot at Mon Jan 26 10:51:59 CET 2015

--- test/FunctionResizeTest.php
+++ test/FunctionResizeTest.php
@@ -3,7 +3,7 @@
 class FunctionResizeTest extends PHPUnit_Framework_TestCase {
     public function testOpts()
     {
-        $this->assertInstanceOf('RuntimeException', new Options);
+        $this->assertInstanceOf('Options', new Options);
     }
 }
 
