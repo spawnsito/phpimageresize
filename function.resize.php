68490ae0ecbdcf9f769333c5116ea71372faa521
Sergio Revilla <replymusic@gmail.com>
2015-02-02 12:20:58 +0100

    snapshot at Mon Feb  2 11:18:26 CET 2015

--- function.resize.php
+++ function.resize.php
@@ -62,23 +62,7 @@ function resize($imagePath,$opts=null){
 	$w = $configuration->obtainWidth();
 	$h = $configuration->obtainHeight();
 
-	$filename = md5_file($imagePath);
-
-	$finfo = pathinfo($imagePath);
-	$ext = $finfo['extension'];
-
-	$cropSignal = isset($opts['crop']) && $opts['crop'] == true ? "_cp" : "";
-	$scaleSignal = isset($opts['scale']) && $opts['scale'] == true ? "_sc" : "";
-	$widthSignal = !empty($w) ? '_w'.$w : '';
-	$heightSignal = !empty($h) ? '_h'.$h : '';
-	$extension = '.'.$ext;
-
-	$newPath = $configuration->obtainCache() .$filename.$widthSignal.$heightSignal.$cropSignal.$scaleSignal.$extension;
-
-	if($opts['output-filename']) {
-		$newPath = $opts['output-filename'];
-	}
-
+	$newPath = composeNewPath($imagePath, $configuration);
 	if(empty($opts['output-filename']) && empty($w) && empty($h)) {
 		return 'cannot resize the image';
 	}
