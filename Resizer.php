9ef31e294d786b82c73d2d32370cb7396b927728
Sergio Revilla <replymusic@gmail.com>
2015-02-02 11:33:34 +0100

    snapshot at Mon Feb  2 11:18:26 CET 2015

--- Resizer.php
+++ Resizer.php
@@ -30,7 +30,7 @@ class Resizer {
             $inCache = $this->isInCache($local_filepath);
 
             if(!$inCache):
-                $this->download($imagePath, $local_filepath);
+                $this->download($filename, $local_filepath);
             endif;
             $imagePath = $local_filepath;
         endif;
