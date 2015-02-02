b04bade72aec8350bbd407500d9d5012ce30f687
Sergio Revilla <replymusic@gmail.com>
2015-02-02 12:08:02 +0100

    snapshot at Mon Feb  2 11:18:26 CET 2015

--- function.resize.php
+++ function.resize.php
@@ -35,6 +35,7 @@ function resize($imagePath,$opts=null){
 	$scaleSignal = isset($opts['scale']) && $opts['scale'] == true ? "_sc" : "";
 	$widthSignal = !empty($w) ? '_w'.$w : '';
 	$heightSignal = !empty($h) ? '_h'.$h : '';
+	$extension = '.'.$ext;
 	$newPath = $configuration->obtainCache() .$filename.$widthSignal.$heightSignal.$cropSignal.$scaleSignal.$extension;
 
 	if(false !== $opts['output-filename']) :
